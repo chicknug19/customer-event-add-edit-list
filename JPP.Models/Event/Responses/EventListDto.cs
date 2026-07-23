a107793de6f1962e1769e3bbe09f47ae7a974862
Jason Benoit Adianto <jason.adianto@binus.edu>
2026-07-23 16:39:43 +0700

    adj event list

--- JPP.Models/Event/Responses/EventListDto.cs
+++ JPP.Models/Event/Responses/EventListDto.cs
@@ -6,5 +6,11 @@ namespace JPP.Models.Event.Responses
         public string EventName { get; set; } = string.Empty;
         public string Code { get; set; } = string.Empty;
         public string Description { get; set; } = string.Empty;
+        public string Location { get; set; } = string.Empty;
+        public string DatabaseName { get; set; } = string.Empty;
+        public string Brand { get; set; } = string.Empty;
+        public string EventOrganizer { get; set; } = string.Empty;
+        public DateTime? EventDateTime { get; set; }
+        public decimal Duration { get; set; }
     }
 }
